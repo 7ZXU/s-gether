d5c426afbbab5998214ed4d26bc2301eaf0e538d
7zxu <jisu7935@g.skku.edu>
2021-11-01 19:41:44 +0900

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-<<<<<<< HEAD
 import React from "react";
-import {Route, Switch} from "react-router-dom";
+import "../css/App.css";
+import { Route, Switch } from "react-router-dom";
 import FeedPage from "./pages/FeedPage";
 import MainPage from "./pages/MainPage";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 import ImagePage from "./pages/ImagePage";
 
-function App() {
-    return (
-        <div className="App">
-            <Switch>
-                <Route exact path="/" component={MainPage}/>
-                <Route exact path="/feed" component={FeedPage}/>
-                <Route exact path="/feed/image" component={ImagePage}/>
-            </Switch>
-        </div>
-    );
-=======
-import './App.css';
-import Main from './Pages/Main';
-import Login from './Pages/Login';
-import MyPage from './Pages/MyPage';
-import Register from './Pages/Register';
+import ChallengePage from "./pages/ChallengePage";
 
 function App() {
   return (
-    <>
-      <MyPage />
-    </>
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/feed" component={FeedPage} />
+        <Route exact path="/feed/image" component={ImagePage} />
+        <Route exact path="/challenges" component={ChallengePage} />
+      </Switch>
+    </div>
   );
->>>>>>> main
 }
 
 export default App;
