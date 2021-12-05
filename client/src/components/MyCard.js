a9d213195f6c5a65a233228e052cec5ea2ac5227
PSYcode04 <psy.code04@gmail.com>
2021-12-05 17:49:11 +0900

    - 챌린지 시작 날짜가 미래의 날짜일 경우 챌린지 페이지로 넘어가지 않고 팝업창 띄우기

--- client/src/components/MyCard.js
+++ client/src/components/MyCard.js
@@ -3,13 +3,14 @@ import Item from './item';
 import Carousel from 'react-elastic-carousel';
 import Typography from '@mui/material/Typography';
 import studyBackground from '../assets/studyBackground.jpg';
-import { setCookie, getCookie } from '../cookie';
+import { getCookie } from '../cookie';
 import jwt from 'jsonwebtoken';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import '../css/card.css';
 import { formControlUnstyledClasses } from '@mui/core';
 import SpecificItem from './specificItem';
+import NoticeModal from './NoticeModal';
 
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
@@ -17,10 +18,19 @@ const breakPoints = [
   { width: 768, itemsToShow: 3 },
   { width: 1200, itemsToShow: 4 },
 ];
-
 function MyCard() {
   const [dataList, setdata] = useState([]);
   const token = getCookie('myToken');
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   async function callChallengeList() {
     console.log('call challenge');
@@ -42,38 +52,71 @@ function MyCard() {
     callChallengeList();
   }, []);
 
+  // 데이터 값으로 변수 설정
+  const now = new Date();
+
   if (dataList.length !== 0) {
     return (
       <div className="card">
         <Carousel breakPoints={breakPoints}>
-          {dataList.length !== 0 ? (
-            dataList.map((data, key) => {
-              const url = `/myChallenge/:${data.id}`;
-              return (
-                <SpecificItem key={key}>
-                  <Link to={url}>
-                    <img
-                      className="studyBackground-img"
-                      src={data['img']}
-                      alt="card 사진"
-                      heigth="200"
-                      width="200"
-                      align="center"
-                    />
-                  </Link>
-                  <Typography variant="h6" component="div" align="center">
-                    {data['name']}
-                  </Typography>
-                  <Typography variant="h6" component="div" align="center">
-                    Date : {data['startDate'].split('T')[0]} ~{' '}
-                    {data['endDate'].split('T')[0]}
-                  </Typography>
-                </SpecificItem>
-              );
-            })
-          ) : (
-            <p>현재 참여중인 챌린지가 없습니다.</p>
-          )}
+          {dataList.length !== 0
+            ? dataList.map((data, key) => {
+                const url = `/myChallenge/:${data.id}`;
+                const startDate = new Date(data.startDate);
+                console.log(now < startDate ? '시작 전입니다.' : '시작가능');
+                if (now < startDate) {
+                  return (
+                    <SpecificItem key={key}>
+                      <img
+                        className="studyBackground-img"
+                        src={data['img']}
+                        alt="card 사진"
+                        heigth="200"
+                        width="200"
+                        align="center"
+                        style={{ cursor: 'pointer' }}
+                        onClick={openModal}
+                      />
+                      <NoticeModal
+                        open={modalOpen}
+                        close={closeModal}
+                        setText="아직 스터디 시작 날짜가 아니에요 ^^"
+                        check="확인"
+                      ></NoticeModal>
+                      <Typography variant="h6" component="div" align="center">
+                        {data['name']}
+                      </Typography>
+                      <Typography variant="h6" component="div" align="center">
+                        Date : {data['startDate'].split('T')[0]} ~{' '}
+                        {data['endDate'].split('T')[0]}
+                      </Typography>
+                    </SpecificItem>
+                  );
+                } else {
+                  return (
+                    <SpecificItem key={key}>
+                      <Link to={url}>
+                        <img
+                          className="studyBackground-img"
+                          src={data['img']}
+                          alt="card 사진"
+                          heigth="200"
+                          width="200"
+                          align="center"
+                        />
+                      </Link>
+                      <Typography variant="h6" component="div" align="center">
+                        {data['name']}
+                      </Typography>
+                      <Typography variant="h6" component="div" align="center">
+                        Date : {data['startDate'].split('T')[0]} ~{' '}
+                        {data['endDate'].split('T')[0]}
+                      </Typography>
+                    </SpecificItem>
+                  );
+                }
+              })
+            : null}
         </Carousel>
       </div>
     );
