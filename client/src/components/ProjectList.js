8794eeb7a2ec98c688f82ccff71e6fb1a9e3406a
jinhwan <may3210@naver.com>
2021-12-06 00:17:31 +0900

    Challenge Fix

--- client/src/components/ProjectList.js
+++ client/src/components/ProjectList.js
@@ -34,13 +34,19 @@ function ProjectList(props) {
         setCard(rows[key])
       
     }
+    var now = new Date();
+
     for(var i = 0; i < dataList.length; i++){
         const data = dataList[i]
-        if(data['category'] === "Project"){
-            
-            if(typeof data['startDate'] != "undefined" && typeof data['endDate'] != "undefined" ){
-                rows.push(createData(data['img'], data['name'],  data['startDate'].split('T')[0], data['endDate'].split('T')[0], data['max_participants'], 
-                data['current_participants'], data['fee'], data['id']))
+        
+        if(data['category'] === "Project" ){
+            const startDate = new Date(data['startDate']);
+            const endDate = new Date(data['endDate']);
+            if(startDate < now && endDate > now){
+                if(typeof data['startDate'] != "undefined" && typeof data['endDate'] != "undefined" ){
+                    rows.push(createData(data['img'],data['name'],  data['startDate'].split('T')[0], data['endDate'].split('T')[0], data['max_participants'], 
+                    data['current_participants'],data['fee'], data['id']))
+                }
             }
         }
     }
--- client/src/components/WorkoutList.js
+++ client/src/components/WorkoutList.js
@@ -35,15 +35,19 @@ function WorkoutList(props) {
         setCard(rows[key])
         console.log(rows[key])
     }
+    let now = new Date();
 
     for(var i = 0; i < dataList.length; i++){
         const data = dataList[i]
         
         if(data['category'] === "Workout"){
-            
-            if(typeof data['startDate'] != "undefined" && typeof data['endDate'] != "undefined" ){
-                rows.push(createData(data['img'],data['name'],  data['startDate'].split('T')[0], data['endDate'].split('T')[0], data['max_participants'], 
-                data['current_participants'],data['fee'], data['id']))
+            const startDate = new Date(data['startDate']);
+            const endDate = new Date(data['endDate']);
+            if(startDate < now && endDate > now){
+                if(typeof data['startDate'] != "undefined" && typeof data['endDate'] != "undefined" ){
+                    rows.push(createData(data['img'],data['name'],  data['startDate'].split('T')[0], data['endDate'].split('T')[0], data['max_participants'], 
+                    data['current_participants'],data['fee'], data['id']))
+                }
             }
         }
     }
